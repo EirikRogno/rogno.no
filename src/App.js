9a56c258288543216295a77090fee1202225f611
Eirik Rognø <eirik.rogno@gmail.com>
2018-05-19 22:51:20 +0200

    Add links to SoMe

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './App.css';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
+import faLinkedin from '@fortawesome/fontawesome-free-brands/faGithub';
+import faGithub from '@fortawesome/fontawesome-free-brands/faLinkedin';
 
 const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title"> Hello World!</h1>
-        </header>
-        <p className="App-intro">
-          Hei
-        </p>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <p className="App-title"> Eirik Rognø </p>
+          </header>
+          <div className="Icon-row">
+			<a href="https://github.com/EirikRogno"> 
+				<FontAwesomeIcon icon={faGithub} className="Icon fa-3x" /> 
+			</a>
+			<a href="https://www.linkedin.com/in/eirik-rognø-413983105"> 
+				<FontAwesomeIcon icon={faLinkedin} className="Icon fa-3x" /> 
+			</a>
+			<a href="mailto:eirik@rogno.no"> 
+				<FontAwesomeIcon icon={faEnvelope} className="Icon fa-3x" /> 
+			</a>
+          </div>
+        </div>
     );
 }
 
